ab0047d46899575f8f856e9e4342a39a9dd79094
Alex <light.kharkiv@gmail.com>
2018-12-31 12:05:19 +0200

    nginx & sequre log check

--- log_check_v1.sh
+++ log_check_v1.sh
@@ -2,16 +2,102 @@
 LOG_FILE="/var/log/nginx/access.log"
 CARRENT_DATE=$(date +"%d")
 cat $LOG_FILE | grep $CARRENT_DATE | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" > CARRENT_IP.txt
-FILE_IP="CARRENT_IP.txt"
+sort CARRENT_IP.txt > CARRENT_SORT_IP.txt && uniq CARRENT_SORT_IP.txt > UNIQ_IP.txt
+FILE_IP="UNIQ_IP.txt"
 echo $LOG_FILE > report.txt
 echo "===================================================================================================" >> report.txt
 for DISCOVER_IP in $(cat $FILE_IP)
 do
 echo $DISCOVER_IP >> report.txt
-whois $DISCOVER_IP | grep -i country >> report.txt
-whois $DISCOVER_IP | grep -i descr >> report.txt
-#whois $DISCOVER_IP | grep -i OrgName >> report.txt
-whois $DISCOVER_IP | grep -i Organization >> report.txt
+whois $DISCOVER_IP | grep -i -e country -i -e descr -i -e OrgName -i -e Org-Name -i -e Organization >> report.txt
 echo " " >> report.txt
 done
-echo Коннект за сутки | mutt -s "Суточный отчет" alex.lanovoy@gmail.com -a /var/log/dev/report.txt
+
+LOG_FILE="/var/log/nginx/error.log"
+CARRENT_DATE=$(date +"%d")
+cat $LOG_FILE | grep $CARRENT_DATE | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" > CARRENT_IP.txt
+sort CARRENT_IP.txt > CARRENT_SORT_IP.txt && uniq CARRENT_SORT_IP.txt > UNIQ_IP.txt
+FILE_IP="UNIQ_IP.txt"
+echo $LOG_FILE >> report.txt
+echo "===================================================================================================" >> report.txt
+for DISCOVER_IP in $(cat $FILE_IP)
+do
+echo $DISCOVER_IP >> report.txt
+whois $DISCOVER_IP | grep -i -e country -i -e descr -i -e OrgName -i -e Org-Name -i -e Organization >> report.txt
+echo " " >> report.txt
+done
+
+LOG_FILE="/var/log/nginx/lameya-access.log"
+CARRENT_DATE=$(date +"%d")
+cat $LOG_FILE | grep $CARRENT_DATE | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" > CARRENT_IP.txt
+sort CARRENT_IP.txt > CARRENT_SORT_IP.txt && uniq CARRENT_SORT_IP.txt > UNIQ_IP.txt
+FILE_IP="UNIQ_IP.txt"
+echo $LOG_FILE >> report.txt
+echo "===================================================================================================" >> report.txt
+for DISCOVER_IP in $(cat $FILE_IP)
+do
+echo $DISCOVER_IP >> report.txt
+whois $DISCOVER_IP | grep -i -e country -i -e descr -i -e OrgName -i -e Org-Name -i -e Organization >> report.txt
+echo " " >> report.txt
+done
+
+LOG_FILE="/var/log/nginx/lameya-error.log"
+CARRENT_DATE=$(date +"%d")
+cat $LOG_FILE | grep $CARRENT_DATE | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" > CARRENT_IP.txt
+sort CARRENT_IP.txt > CARRENT_SORT_IP.txt && uniq CARRENT_SORT_IP.txt > UNIQ_IP.txt
+FILE_IP="UNIQ_IP.txt"
+echo $LOG_FILE >> report.txt
+echo "===================================================================================================" >> report.txt
+for DISCOVER_IP in $(cat $FILE_IP)
+do
+echo $DISCOVER_IP >> report.txt
+whois $DISCOVER_IP | grep -i -e country -i -e descr -i -e OrgName -i -e Org-Name -i -e Organization >> report.txt
+echo " " >> report.txt
+done
+
+LOG_FILE="/var/log/nginx/download.log"
+CARRENT_DATE=$(date +"%d")
+cat $LOG_FILE | grep $CARRENT_DATE | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" > CARRENT_IP.txt
+sort CARRENT_IP.txt > CARRENT_SORT_IP.txt && uniq CARRENT_SORT_IP.txt > UNIQ_IP.txt
+FILE_IP="UNIQ_IP.txt"
+echo $LOG_FILE >> report.txt
+echo "===================================================================================================" >> report.txt
+cat CARRENT_SORT_IP.txt >> report.txt
+for DISCOVER_IP in $(cat $FILE_IP)
+do
+echo $DISCOVER_IP >> report.txt
+whois $DISCOVER_IP | grep -i -e country -i -e descr -i -e OrgName -i -e Org-Name -i -e Organization >> report.txt
+echo " " >> report.txt
+done
+
+LOG_FILE="/var/log/nginx/download-error.log"
+CARRENT_DATE=$(date +"%d")
+cat $LOG_FILE | grep $CARRENT_DATE | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" > CARRENT_IP.txt
+sort CARRENT_IP.txt > CARRENT_SORT_IP.txt && uniq CARRENT_SORT_IP.txt > UNIQ_IP.txt
+FILE_IP="UNIQ_IP.txt"
+echo $LOG_FILE >> report.txt
+echo "===================================================================================================" >> report.txt
+for DISCOVER_IP in $(cat $FILE_IP)
+do
+echo $DISCOVER_IP >> report.txt
+whois $DISCOVER_IP | grep -i -e country -i -e descr -i -e OrgName -i -e Org-Name -i -e Organization >> report.txt
+echo " " >> report.txt
+done
+
+LOG_FILE="/var/log/secure"
+CARRENT_DATE=$(date +"%d")
+cat $LOG_FILE | grep $CARRENT_DATE | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" > CARRENT_IP.txt
+sort CARRENT_IP.txt > CARRENT_SORT_IP.txt && uniq CARRENT_SORT_IP.txt > UNIQ_IP.txt
+FILE_IP="UNIQ_IP.txt"
+echo $LOG_FILE >> report.txt
+echo "===================================================================================================" >> report.txt
+for DISCOVER_IP in $(cat $FILE_IP)
+do
+echo $DISCOVER_IP >> report.txt
+whois $DISCOVER_IP | grep -i -e country -i -e descr -i -e OrgName -i -e Org-Name -i -e Organization >> report.txt
+echo " " >> report.txt
+done
+
+
+cat report.txt
+#echo Коннект за сутки | mutt -s "Суточный отчет" alex.lanovoy@gmail.com -a /var/log/dev/report.txt
